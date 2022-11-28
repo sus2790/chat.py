b31f61c7a806ceaa7e2042215ce8dc3bea808047
ItsRqtl <itsrqtl@gmail.com>
2022-11-27 21:51:46 -0800

    feat: implement sending in threads

--- chat/webhook.py
+++ chat/webhook.py
@@ -1,40 +1,38 @@
-"""
-The MIT License (MIT)
-
-Copyright (c) 2022-present sus2790
-Copyright (c) 2020-present shay (shayypy)
-Copyright (c) 2021-present Pycord Development
-
-Permission is hereby granted, free of charge, to any person obtaining a
-copy of this software and associated documentation files (the "Software"),
-to deal in the Software without restriction, including without limitation
-the rights to use, copy, modify, merge, publish, distribute, sublicense,
-and/or sell copies of the Software, and to permit persons to whom the
-Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-DEALINGS IN THE SOFTWARE.
-"""
-
-from __future__ import annotations
+from typing import Optional
 
 import aiohttp
 
+from .exception import SendException
+
+__all__ = ("Webhook",)
+
 
 class Webhook:
+    """
+    A class representing a Google Chat webhook.
+
+    :param url: The URL of the webhook.
+    :type url: str
+
+    :ivar url: The URL of the webhook.
+    """
+
     def __init__(self, url: str = None) -> None:
         self.url = url
 
-    async def send(self, content: str):
-        # TODO: Finish fetch.py
+    async def send(self, content: str, thread_id: Optional[str] = None) -> None:
+        """
+        Send a message to the webhook.
+
+        :param content: The content of the message.
+        :type content: str
+        :param thread_id?: The thread ID of the message (if any).
+        :type thread_id?: str
+        """
+        url = self.url + "&threadKey=" + thread_id if thread_id else self.url
+        json = {"text": content}
         async with aiohttp.ClientSession() as session:
-            async with session.post(self.url, json={"text": content}):
-                return
+            async with session.post(url, json=json) as resp:
+                data = await resp.json()
+                if "error" in data:
+                    raise SendException(data["error"]["message"])
