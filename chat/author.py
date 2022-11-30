f0423506594dac17a9ca2c0beb3a6b0ad2be2f74
sus2790 <104747821+sus2790@users.noreply.github.com>
2022-11-30 17:14:44 +0800

    Add files via upload

---
+++ chat/author.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+
+
+class Author(object):
+    def __init__(self, data: str) -> None:
+        self._data = data
+
+    @property
+    def name(self):
+        return self._data["sender"]["displayName"]
+
+    @property
+    def type(self):
+        return self._data["sender"]["type"]
+
+    @property
+    def avatar_url(self):
+        return self._data["sender"]["avatarUrl"]
+
+    @property
+    def created_at(self):
+        return self._data["createTime"]
---
+++ chat/guild.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+
+
+class Guild(object):
+    def __init__(self, data: str) -> None:
+        self._data = data
+
+    @property
+    def name(self):
+        return self._data["space"]["displayName"]
+
+    @property
+    def type(self):
+        return self._data["space"]["type"]
+
+    @property
+    def rule(self):
+        return self._data["spaceDetails"]["guidelines"]
+
+    @property
+    def description(self):
+        return self._data["spaceDetails"]["description"]
