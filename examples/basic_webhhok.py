0454ee34a18f8b557b991c72c6aaf576a9b89f17
sus2790 <104747821+sus2790@users.noreply.github.com>
2022-12-10 22:30:53 +0800

    Update basic_webhhok.py

--- examples/basic_webhhok.py
+++ examples/basic_webhhok.py
@@ -1,14 +1,18 @@
 import asyncio
+
 from chat.webhook import Webhook
-#導入這個函式庫
+
+# 導入這個函式庫
 
 webhook = Webhook("網址")
-#設定webhook網址
+# 設定webhook網址
+
 
 async def foo():
     message = await webhook.send("傑哥不要啦！")
-    #顯示發送的訊息
+    # 顯示發送的訊息
     print(message.content)
 
-#開始運行webhook  
+
+# 開始運行webhook
 asyncio.run(foo())
